8aa162aad92da706f956ae5bc1d82f16c545254c
LilCalc <noahsavinois@gmail.com>
2022-06-26 20:05:30 -0500

    Attempt at getting heroku to deploy

--- config/connection.js
+++ config/connection.js
@@ -12,6 +12,9 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
+    dialectOptions: {
+      socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
+    },  
     port: 3306
   });
 }
